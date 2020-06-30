3dd68feb9238a4d80d714e87ccd78e59a04e6a3b
yejun <1114126929@qq.com>
2020-06-30 19:05:13 +0800

    fix

--- src/views/wel/index.vue
+++ src/views/wel/index.vue
@@ -4,11 +4,11 @@
     <basic-container>
       值:{{form1}}
       <br />
-      <avue-input v-model="form" placeholder="请输入内容"></avue-input>
+      <avue-input v-model="form1" placeholder="请输入内容"></avue-input>
     </basic-container>
 
     <!-- inputNumber -->
-    <basic-container>
+    <!-- <basic-container>
       <el-row :span="24">
         <el-col :span="6">
           值:{{form2}}
@@ -22,7 +22,7 @@
           <avue-input-number v-model="form2" precision="2" controls-position></avue-input-number>
         </el-col>
       </el-row>
-    </basic-container>
+    </basic-container> -->
 
     <!-- select下拉菜单 -->
     <basic-container>
@@ -173,21 +173,27 @@
 
 <script>
 var DIC = {
-    VAILD: [{
-        label: '真',
-        value: 'true'
-    }, {
-        label: '假',
-        value: 'false'
-    }],
-    SEX: [{
-        label: '男',
-        value: 0
-    }, {
-        label: '女',
-        value: 1
-    }]
-}
+  VAILD: [
+    {
+      label: "真",
+      value: "true"
+    },
+    {
+      label: "假",
+      value: "false"
+    }
+  ],
+  SEX: [
+    {
+      label: "男",
+      value: 0
+    },
+    {
+      label: "女",
+      value: 1
+    }
+  ]
+};
 import { mapGetters } from "vuex";
 export default {
   name: "wel",
@@ -231,12 +237,8 @@ export default {
       ],
       form5: "2020-01-01",
       form6: "2020-01-01 12:00:00",
-      obj:{
-        username :'smallwei',
-        switch:0,
-        phone:'17547400800'
-      },
-      sizeValue:'small'
+      obj: {},
+      sizeValue: "small"
     };
   },
   computed: {
@@ -398,194 +400,207 @@ export default {
         ]
       };
     },
-    option(){
-         return{
-          size:this.sizeValue,
-          mockBtn:true,
-          submitText: '完成',
-          printBtn:true,
-          column: [{
-                  label: "用户名",
-                  prop: "username",
-                  tip: '这是信息提示',
-                  span: 8,
-                  maxlength: 3,
-                  suffixIcon: 'el-icon-tickets',
-                  prefixIcon: 'el-icon-tickets',
-                  minlength: 2,
-                    mock:{
-                    type:'name',
-                    en:true,
-                  },
-                  rules: [{
-                      required: true,
-                      message: "请输入用户名",
-                      trigger: "blur"
-                  }],
-                  change:({value,column})=>{
-                      this.obj.address=value;
-                      this.$message.success('address change')
-                  },
-                  click:({value,column})=>{
-                      this.$message.success('click')
-                  }
-              },
+    option() {
+      return {
+        size: this.sizeValue,
+        mockBtn: true,
+        submitText: "完成",
+        printBtn: true,
+        column: [
+          {
+            label: "用户名",
+            prop: "username",
+            tip: "这是信息提示",
+            span: 8,
+            maxlength: 3,
+            suffixIcon: "el-icon-tickets",
+            prefixIcon: "el-icon-tickets",
+            minlength: 2,
+            mock: {
+              type: "name",
+              en: true
+            },
+            rules: [
               {
-                  label: "姓名",
-                  prop: "name",
-                  mock:{
-                    type:'name'
-                  },
-                  span:8
-              },
-              {
-                  label: "类型",
-                  prop: "type",
-                  type: "select",
-                  dicData: DIC.VAILD,
-                  span:6,
-                  mock:{
-                    type:'dic',
-                  },
-              },
-              {
-                  label: "权限",
-                  prop: "grade",
-                  span: 6,
-                  type: "checkbox",
-                  dicData: DIC.VAILD,
-                  mock:{
-                    type:'dic',
-                  },
-              },
-              {
-                  label: "开关",
-                  prop: "switch",
-                  span: 6,
-                  type: "switch",
-                  dicData: DIC.SEX,
-                  mock:{
-                    type:'dic'
-                  },
-                  hide: true,
-                  row:true,
-              },
-              {
-                  label: "性别",
-                  prop: "sex",
-                  span: 6,
-                  type: "radio",
-                  dicData: DIC.SEX,
-                  mock:{
-                    type:'dic'
-                  },
-                  change:({value,column})=>{
-                      this.$message.success('change')
-                  }
-              },
-              {
-                  label: "数字",
-                  prop: "number",
-                  type: 'number',
-                  span: 6,
-                  precision:2,
-                  mock:{
-                    type:'number',
-                    max:1,
-                    min:2,
-                    precision:2
-                  },
-                  minRows: 0,
-                  maxRows: 3,
-                  row:true,
-              },
-              {
-                  label: "网站",
-                  span: 12,
-                  prop: "url",
-                  prepend:'http://',
-                  mock:{
-                    type:'url',
-                    header:false,
-                  },
-                  append:'com',
-                  row:true,
-              },
-              {
-                  label: "日期",
-                  prop: "date",
-                  type: "date",
-                  span:8,
-                  format:'yyyy-MM-dd',
-                  valueFormat:'yyyy-MM-dd',
-                  mock:{
-                    type:'datetime',
-                    format:'yyyy-MM-dd'
-                  },
-              },
-              {
-                  label: "日期时间",
-                  prop: "datetime",
-                  type: "datetime",
-                  span:8,
-                  format:'yyyy-MM-dd hh:mm:ss',
-                  valueFormat:'yyyy-MM-dd hh:mm:ss',
-                  mock:{
-                    type:'datetime',
-                    format:'yyyy-MM-dd hh:mm:ss',
-                    now:true,
-                  },
-              },
-                {
-                  label: "时间",
-                  prop: "time",
-                  type: "time",
-                  span:8,
-                  format:'hh:mm:ss',
-                  valueFormat:'hh:mm:ss',
-                  mock:{
-                    type:'datetime',
-                    format:'hh:mm:ss'
-                  },
-              },
-              {
-                  label: "地址",
-                  span: 24,
-                  prop: "address",
-                  mock:{
-                    type:'county'
-                  },
-              },{
-                  label: "建议",
-                  span: 24,
-                  prop: "adit",
-                  mock:{
-                    type:'word',
-                    min:10,
-                    max:30
-                  },
-              },{
-                  label: "手机号",
-                  mock:{
-                    type:'phone'
-                  },
-                  span: 12,
-                  prop: "phone",
-              }]
+                required: true,
+                message: "请输入用户名",
+                trigger: "blur"
+              }
+            ],
+            change: ({ value, column }) => {
+              this.obj.address = value;
+              this.$message.success("address change");
+            },
+            click: ({ value, column }) => {
+              this.$message.success("click");
+            }
+          },
+          {
+            label: "姓名",
+            prop: "name",
+            mock: {
+              type: "name"
+            },
+            span: 8
+          },
+          {
+            label: "类型",
+            prop: "type",
+            type: "select",
+            dicData: DIC.VAILD,
+            span: 6,
+            mock: {
+              type: "dic"
+            }
+          },
+          {
+            label: "权限",
+            prop: "grade",
+            span: 6,
+            type: "checkbox",
+            dicData: DIC.VAILD,
+            mock: {
+              type: "dic"
+            }
+          },
+          {
+            label: "开关",
+            prop: "switch",
+            span: 6,
+            type: "switch",
+            dicData: DIC.SEX,
+            mock: {
+              type: "dic"
+            },
+            hide: true,
+            row: true
+          },
+          {
+            label: "性别",
+            prop: "sex",
+            span: 6,
+            type: "radio",
+            dicData: DIC.SEX,
+            mock: {
+              type: "dic"
+            },
+            change: ({ value, column }) => {
+              this.$message.success("change");
+            }
+          },
+          {
+            label: "数字",
+            prop: "number",
+            type: "number",
+            span: 6,
+            precision: 2,
+            mock: {
+              type: "number",
+              max: 1,
+              min: 2,
+              precision: 2
+            },
+            minRows: 0,
+            maxRows: 3,
+            row: true
+          },
+          {
+            label: "网站",
+            span: 12,
+            prop: "url",
+            prepend: "http://",
+            mock: {
+              type: "url",
+              header: false
+            },
+            append: "com",
+            row: true
+          },
+          {
+            label: "日期",
+            prop: "date",
+            type: "date",
+            span: 8,
+            format: "yyyy-MM-dd",
+            valueFormat: "yyyy-MM-dd",
+            mock: {
+              type: "datetime",
+              format: "yyyy-MM-dd"
+            }
+          },
+          {
+            label: "日期时间",
+            prop: "datetime",
+            type: "datetime",
+            span: 8,
+            format: "yyyy-MM-dd hh:mm:ss",
+            valueFormat: "yyyy-MM-dd hh:mm:ss",
+            mock: {
+              type: "datetime",
+              format: "yyyy-MM-dd hh:mm:ss",
+              now: true
+            }
+          },
+          {
+            label: "时间",
+            prop: "time",
+            type: "time",
+            span: 8,
+            format: "hh:mm:ss",
+            valueFormat: "hh:mm:ss",
+            mock: {
+              type: "datetime",
+              format: "hh:mm:ss"
+            }
+          },
+          {
+            label: "地址",
+            span: 24,
+            prop: "address",
+            mock: {
+              type: "county"
+            }
+          },
+          {
+            label: "建议",
+            span: 24,
+            prop: "adit",
+            mock: {
+              type: "word",
+              min: 10,
+              max: 30
+            }
+          },
+          {
+            label: "手机号",
+            mock: {
+              type: "phone"
+            },
+            span: 12,
+            prop: "phone"
           }
-       }
+        ]
+      };
+    }
   },
   created() {},
+  mounted() {
+    this.obj={
+      username :'smallwei',
+      switch:0,
+      phone:'17547400800'
+    }
+  },
   methods: {
-    emptytChange(){
-          this.$message.success('清空方法回调');
-        },
-        submit () {
-          this.$message.success('当前数据'+JSON.stringify(this.obj));
-        },
-        tip(){
-          this.$message.success('自定义按钮');
-        }
+    emptytChange() {
+      this.$message.success("清空方法回调");
+    },
+    submit() {
+      this.$message.success("当前数据" + JSON.stringify(this.obj));
+    },
+    tip() {
+      this.$message.success("自定义按钮");
+    }
   }
 };
 </script>
